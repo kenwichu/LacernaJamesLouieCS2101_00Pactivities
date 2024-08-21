0ed30d4477c6eb0a3fb4f8a776ad5d30bd7563c2
kchu <162998252+kenwichu@users.noreply.github.com>
2024-08-21 09:57:26 +0800

    Update Escseq.java

--- LacernaJamesLouieCS2101_OOPactivities/Exercise Set 01/Escseq.java
+++ LacernaJamesLouieCS2101_OOPactivities/Exercise Set 01/Escseq.java
@@ -1,1 +1,15 @@
+public class Escseq {
+    public static void main(String[] args) {
+        // Loop to print multiples of 7
+        for (int i = 1; i <= 5; i++) {
+            int multiple = i * 7;
+            // Print the number followed by either a tab or a new line
+            if (i % 2 == 0) {
+                System.out.println("\t" + multiple);
+            } else {
+                System.out.println(multiple);
+            }
+        }
+    }
+}
 
