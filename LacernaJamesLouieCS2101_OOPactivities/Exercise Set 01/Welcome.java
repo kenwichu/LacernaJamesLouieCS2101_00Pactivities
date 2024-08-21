d99774cfd4d792c52bb3057b78e72f1266b07ecd
kchu <162998252+kenwichu@users.noreply.github.com>
2024-08-21 09:55:13 +0800

    Update Welcome.java

--- LacernaJamesLouieCS2101_OOPactivities/Exercise Set 01/Welcome.java
+++ LacernaJamesLouieCS2101_OOPactivities/Exercise Set 01/Welcome.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CS 211: Object-oriented Programming!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("This course will be fun, " + name + "!");
+        
+        scanner.close();
+    }
+}
 
