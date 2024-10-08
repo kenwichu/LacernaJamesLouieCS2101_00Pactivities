7f43cc578bf9a7d0767bcc6c6d183c8111f7a2b5
kchu <162998252+kenwichu@users.noreply.github.com>
2024-10-08 17:07:47 +0800

    Update Car.java

--- LacernaJamesLouieCS2101_OOPactivities/Exercise Set 02/Car.java
+++ LacernaJamesLouieCS2101_OOPactivities/Exercise Set 02/Car.java
@@ -1,1 +1,65 @@
+public class Car {
+    private String color;
+    private double price;
+    private char size;
+
+    public Car(String color, double price, char size) {
+        this.color = color;
+        this.price = price;
+        this.size = Character.toUpperCase(size);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setSize(char size) {
+        this.size = Character.toUpperCase(size);
+    }
+
+    public String toString() {
+        String sizeDescriptor;
+        switch (size) {
+            case 'S':
+                sizeDescriptor = "small";
+                break;
+            case 'M':
+                sizeDescriptor = "medium";
+                break;
+            case 'L':
+                sizeDescriptor = "large";
+                break;
+            default:
+                sizeDescriptor = "unknown";
+                break;
+        }
+
+        return String.format("Car (%s) - P%.2f - %s", color, price, sizeDescriptor);
+    }
+
+    public static void main(String[] args) {
+        // Example 1: Red car, priced at 19999.85, medium size
+        Car car1 = new Car("red", 19999.85, 'M');
+        System.out.println(car1);
+
+        Car car2 = new Car("blue", 50000.00, 'L');
+        System.out.println(car2);
+    }
+}
 
