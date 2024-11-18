796efabd6bc254f89a636cbc928b23a0f8a3119e
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-18 21:47:24 +0800

    Update ArtistDemo.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/ArtistDemo.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/ArtistDemo.java
@@ -1,1 +1,30 @@
+public class ArtistDemo {
+    public static void main(String[] args) {
+        // Artist instance
+        Artist artist = new Artist("Charlie Puth", 32, "Music");
+        System.out.println("Artist Name: " + artist.getName());
+        System.out.println("Age: " + artist.getAge());
+        System.out.println("Specialty: " + artist.getSpecialty());
+        System.out.println();
+
+        // Singer instance
+        Singer singer = new Singer("Ariana Grande", 31, Singer.Genre.POP);
+        System.out.println(singer.getDetails());
+        System.out.println();
+
+        // Painter instance (corrected)
+        Painter painter = new Painter("Pablo Picasso", 91, Painter.Medium.OIL);
+        System.out.println(painter.getDetails());
+        System.out.println();
+
+        // Writer instance
+        Writer writer = new Writer("Jane Austen", 41, Writer.WritingStyle.FICTION);
+        System.out.println(writer.getDetails());
+        System.out.println();
+
+        // Dancer instance
+        Dancer dancer = new Dancer("La Lisa", 26, Dancer.DanceStyle.HIPHOP);
+        System.out.println(dancer.getDetails());
+    }
+}
 
