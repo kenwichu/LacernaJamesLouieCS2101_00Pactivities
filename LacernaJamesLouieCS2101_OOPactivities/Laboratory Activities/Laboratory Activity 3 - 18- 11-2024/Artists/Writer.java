bb131fb6c6d3b68084feff1edf061ba74f9414b5
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-18 21:49:53 +0800

    Update Writer.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Writer.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Writer.java
@@ -1,1 +1,27 @@
+public class Writer extends Artist {
+    public enum WritingStyle {
+        FICTION, NONFICTION, POETRY, DRAMA
+    }
+
+    private WritingStyle writingStyle;
+
+    public Writer(String name, int age, WritingStyle writingStyle) {
+        super(name, age, "Literature");
+        this.writingStyle = writingStyle;
+    }
+
+    public WritingStyle getWritingStyle() {
+        return writingStyle;
+    }
+
+    public void setWritingStyle(WritingStyle writingStyle) {
+        this.writingStyle = writingStyle;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Writer's Info:\n" +
+               super.getDetails() + "\nWriting Style: " + writingStyle;
+    }
+}
 
