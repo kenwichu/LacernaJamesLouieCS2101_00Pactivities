7b9f9faec0fb86e682ceddee77036a9882e6bcec
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-18 21:48:24 +0800

    Update Dancer.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Dancer.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Dancer.java
@@ -1,1 +1,27 @@
+public class Dancer extends Artist {
+    public enum DanceStyle {
+        BALLET, HIPHOP, JAZZ, CONTEMPORARY
+    }
+
+    private DanceStyle danceStyle;
+
+    public Dancer(String name, int age, DanceStyle danceStyle) {
+        super(name, age, "Dance");
+        this.danceStyle = danceStyle;
+    }
+
+    public DanceStyle getDanceStyle() {
+        return danceStyle;
+    }
+
+    public void setDanceStyle(DanceStyle danceStyle) {
+        this.danceStyle = danceStyle;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Dancer's Info:\n" +
+               super.getDetails() + "\nDance Style: " + danceStyle;
+    }
+}
 
