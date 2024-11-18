feee149959c15a52552234af74c1e29dc42234e4
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-18 21:48:55 +0800

    Update Painter.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Painter.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Painter.java
@@ -1,1 +1,27 @@
+public class Painter extends Artist {
+    public enum Medium {
+        OIL, WATERCOLOR, ACRYLIC, PENCIL, CHARCOAL
+    }
+
+    private Medium medium;
+
+    public Painter(String name, int age, Medium medium) {
+        super(name, age, "Art");
+        this.medium = medium;
+    }
+
+    public Medium getMedium() {
+        return medium;
+    }
+
+    public void setMedium(Medium medium) {
+        this.medium = medium;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Painter's Info:\n" +
+               super.getDetails() + "\nMedium: " + medium;
+    }
+}
 
