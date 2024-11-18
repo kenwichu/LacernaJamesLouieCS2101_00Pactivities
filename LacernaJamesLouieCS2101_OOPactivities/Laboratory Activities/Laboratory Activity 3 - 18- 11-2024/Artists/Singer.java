0235b1743a2d3a7e5ccacc778138b372fd4db014
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-18 21:49:29 +0800

    Update Singer.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Singer.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Singer.java
@@ -1,1 +1,27 @@
+public class Singer extends Artist {
+    public enum Genre {
+        POP, ROCK, CLASSICAL, JAZZ, HIPHOP, RNB
+    }
+
+    private Genre genre;
+
+    public Singer(String name, int age, Genre genre) {
+        super(name, age, "Music");
+        this.genre = genre;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Singer's Info:\n" + 
+               super.getDetails() + "\nGenre: " + genre;
+    }
+}
 
