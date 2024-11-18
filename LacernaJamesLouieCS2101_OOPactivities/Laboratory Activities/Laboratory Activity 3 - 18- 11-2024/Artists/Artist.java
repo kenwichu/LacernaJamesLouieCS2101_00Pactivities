4171d93d5e392f10a022709700f8b6f9c6193084
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-18 21:47:48 +0800

    Update Artist.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Artist.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 18- 11-2024/Artists/Artist.java
@@ -1,1 +1,42 @@
+public class Artist {
+    private String name;
+    private int age;
+    private String specialty;
+
+    public Artist(String name, int age, String specialty) {
+        this.name = name;
+        this.age = age;
+        this.specialty = specialty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    protected void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getDetails() {
+        return "Artist Name: " + name + "\nAge: " + age + "\nSpecialty: " + specialty;
+    }
+}
 
