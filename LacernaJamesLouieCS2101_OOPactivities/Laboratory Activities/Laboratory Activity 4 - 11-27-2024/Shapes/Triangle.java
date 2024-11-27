eeaef60f17406dad50d218b395de027c84bdb235
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-27 08:16:27 +0800

    Update Triangle.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 11-27-2024/Shapes/Triangle.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 11-27-2024/Shapes/Triangle.java
@@ -1,1 +1,26 @@
+public class Triangle implements Shape {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2; // semi-perimeter
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c)); // Heron's formula
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+
+    @Override
+    public void printShapeType() {
+        System.out.println("Triangle");
+    }
+}
 
