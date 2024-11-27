f5867fc67e89d73a864a84e61f70757bfca7c1eb
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-27 08:15:58 +0800

    Update Square.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 11-27-2024/Shapes/Square.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 11-27-2024/Shapes/Square.java
@@ -1,1 +1,23 @@
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public void printShapeType() {
+        System.out.println("Square");
+    }
+}
 
