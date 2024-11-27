003226ce089e1e0879cfca21c24fd782ab6d4d03
kchu <162998252+kenwichu@users.noreply.github.com>
2024-11-27 08:15:34 +0800

    Update ShapeCollection.java

--- LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 11-27-2024/Shapes/ShapeCollection.java
+++ LacernaJamesLouieCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 11-27-2024/Shapes/ShapeCollection.java
@@ -1,1 +1,16 @@
+public class ShapeCollection {
+    private Shape[] shapes;
 
+    public ShapeCollection(Shape[] shapes) {
+        this.shapes = shapes;
+    }
+
+    public void getPropertyValues() {
+        for (Shape shape : shapes) {
+            shape.printShapeType();
+            System.out.printf("Area: %.2f%n", shape.getArea());
+            System.out.printf("Perimeter: %.2f%n", shape.getPerimeter());
+            System.out.println();
+        }
+    }
+}
